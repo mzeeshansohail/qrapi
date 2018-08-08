721595c89abbc1eeaf03a8172f1517804350cbf0
Muhammad Zeeshan Sohail <zeeshansohail17@gmail.com>
2018-08-08 16:23:53 +0500

    javascript to display all map on a page

---
+++ qrapi-frontend/app/scripts/map.js
@@ -0,0 +1,41 @@
+var locations=[];
+accountId= JSON.parse(localStorage.accountId);
+function initMap() {
+
+	$.ajax({
+		url: "http://localhost:8080/qrapi/api/v1/contacts/addresses/"+accountId,
+		type: 'GET',
+		contentType: "application/json; charset=utf-8",
+		dataType: "json",
+		success:
+			function (response, status) { // Required Callback Function
+                //"response" receives - whatever written in echo of above PHP script.
+                data= response;
+                $.each(data,function(i,field){
+                    var location = data[i]['lineOne'] + ' ' + data[i]['lineTwo'] + ' ' + data[i]['city'] + ' ' + data[i]['state'] + ' ' + data[i]['country'];
+                    locations.push(location);
+                })
+                console.log(JSON.stringify(locations));
+                var map = new google.maps.Map(document.getElementById('map'), {
+                    zoom: 8,
+                    center: {lat:33.6844, lng:73.0479}
+                  });    
+                  console.log(locations.length)
+                  for (var x = 0; x <locations.length; x++) {
+                      $.getJSON('http://maps.googleapis.com/maps/api/geocode/json?address='+locations[x]+'&sensor=false', null, function (data) {
+                          var p = data.results[0].geometry.location
+                          var latlng = new google.maps.LatLng(p.lat, p.lng);
+                          new google.maps.Marker({
+                              position: latlng,
+                              map: map
+                          });
+              
+                      }); 
+                    }          
+			},
+		error:
+            function (response, status) { // Required Callback Function
+                console.log(status);
+			}
+	});
+}
