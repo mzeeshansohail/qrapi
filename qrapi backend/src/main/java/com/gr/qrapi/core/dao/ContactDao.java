31754bb9be750e6a599dc30625c350167442d81a
Muhammad Zeeshan Sohail <zeeshansohail17@gmail.com>
2018-08-08 16:30:12 +0500

    ContactDao interface and its implementation

---
+++ qrapi backend/src/main/java/com/gr/qrapi/core/dao/ContactDao.java
@@ -0,0 +1,20 @@
+package com.gr.qrapi.core.dao;
+
+
+import java.util.List;
+
+import com.gr.common.dao.GenericDao;
+import com.gr.qrapi.core.model.Address;
+import com.gr.qrapi.core.model.Contact;
+
+/**
+ * @author ufarooq
+ */
+public interface ContactDao extends GenericDao<Contact, Integer> {
+	List<Contact> getContactByAccountId(int id);
+	boolean deleteContact(int id);
+	boolean updateContact(int id, Contact contact);
+	Contact getContactById(int id);
+	boolean addContact(Contact contact, int id);
+	List<Address> getAddressesByAccountId(int id);
+}
---
+++ qrapi backend/src/main/java/com/gr/qrapi/core/dao/ContactDaoHibernateImpl.java
@@ -0,0 +1,132 @@
+package com.gr.qrapi.core.dao;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.fontbox.util.autodetect.WindowsFontDirFinder;
+import org.hibernate.Criteria;
+import org.hibernate.Session;
+import org.hibernate.criterion.Restrictions;
+import com.gr.common.dao.AbstractHibernateDao;
+import com.gr.common.dao.DaoException;
+import com.gr.common.dao.DaoManager;
+import com.gr.qrapi.core.model.Account;
+import com.gr.qrapi.core.model.Address;
+import com.gr.qrapi.core.model.Contact;
+
+/**
+ * @author ufarooq
+ */
+public class ContactDaoHibernateImpl extends AbstractHibernateDao<Contact, Integer> implements ContactDao {
+	public static ContactDao getDao() {
+		return DaoManager.getInstance().getDao(ContactDao.class);
+	}
+	
+	@SuppressWarnings("unchecked")
+	@Override
+	public List<Contact> getContactByAccountId(int id) {
+		try {
+			Session session= getSession();
+			Criteria criteria = session.createCriteria(Contact.class);
+			criteria.add(Restrictions.eq("account.id", Integer.valueOf(id)));
+			List<Contact> list= (List<Contact>) criteria.list();
+			if (list.size() > 0) {
+	           	System.out.println("Contact exists. returning");
+	           	return list;
+			}
+			else {
+				System.out.println("Contact does not exist.");
+				return null;	
+			}
+		}catch (Exception aex) {
+			throw new DaoException(aex);
+		}
+	}
+	
+
+	@Override
+	public boolean deleteContact(int id) {
+		try {
+			Session session = getSession();
+			Contact contact= new Contact();
+			contact.setId(id);
+			session.delete(contact);
+			return true;
+			}
+			catch (Exception aex) {
+				throw new DaoException(aex);
+			}
+				
+	}	
+	@Override
+	public boolean updateContact(int id, Contact contact) {
+		try {
+			Session session = getSession();
+			contact.setId(id);
+			session.update(contact);
+			return true;
+		}
+			catch(Exception aex) {
+				throw new DaoException(aex);
+			}	
+	}
+
+	@Override
+	public Contact getContactById(int id) {
+		try{
+			Session session= getSession();
+			Contact contact= (Contact) session.get(Contact.class, id);
+		if(contact==null)
+			return null;
+		return contact;
+	}catch (Exception aex) {
+		throw new DaoException(aex);
+	}
+	}
+	
+	
+	public boolean addContact(Contact contact,int id) {
+		try {
+			Session session = getSession();
+			Contact contact1 = new Contact();
+			contact1.setFirstName(contact.getFirstName());
+			contact1.setLastName(contact.getLastName());
+			contact1.setEmail(contact.getEmail());
+			contact1.setGender(contact.getGender());
+			contact1.setPhoneNumber(contact.getPhoneNumber());
+			contact1.setStatus(contact.getStatus());
+			contact1.setAddress(contact.getAddress());
+			Account account = new Account();
+			account= contact.getAccount();
+			account.setId(id);
+			System.out.println("account"+ id);
+			contact1.setAccount(account);
+			session.save(contact1);
+			return true;
+		}catch(Exception aex) {
+			throw new DaoException(aex);
+		}
+	}
+	
+	@SuppressWarnings("unchecked")
+	public List<Address> getAddressesByAccountId(int id) {
+		try {
+			Session session= getSession();
+			Criteria criteria = session.createCriteria(Contact.class);
+			criteria.add(Restrictions.eq("account.id", Integer.valueOf(id)));
+			List<Contact> list= (List<Contact>) criteria.list();
+			System.out.println("List of Contacts"+ list.size());
+			List<Address> listOfAddresses= new ArrayList<Address>();
+			for(int i=0;i< list.size();i++) {
+				listOfAddresses.add(list.get(i).getAddress());
+			}
+			System.out.println("List of Contacts"+ listOfAddresses.size());
+			return listOfAddresses;
+		}catch (Exception aex) {
+			throw new DaoException(aex);
+		}
+	}
+	
+	
+}
+
