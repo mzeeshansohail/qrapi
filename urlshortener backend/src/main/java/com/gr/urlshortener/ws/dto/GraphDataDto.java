208477f4a26b93d08e603eb9f8001abb3a2d333f
Muhammad Zeeshan Sohail <zeeshansohail17@gmail.com>
2018-08-20 12:10:25 +0500

    model class to map the objects with respect to front end- contains labels and data array

---
+++ urlshortener backend/src/main/java/com/gr/urlshortener/ws/dto/GraphDataDto.java
@@ -0,0 +1,30 @@
+package com.gr.urlshortener.ws.dto;
+
+import java.io.Serializable;
+import java.util.List;
+
+
+public class GraphDataDto implements Serializable {
+	private static final long serialVersionUID = 1L;
+	private List<String> labels;
+	private List<Long> data;
+
+	public GraphDataDto() {
+	}
+
+	public List<String> getLabels() {
+		return labels;
+	}
+
+	public void setLabels(List<String> labels) {
+		this.labels = labels;
+	}
+
+	public List<Long> getData() {
+		return data;
+	}
+
+	public void setData(List<Long> data) {
+		this.data = data;
+	}
+}
