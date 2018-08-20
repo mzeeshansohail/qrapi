373ba90f02bc0db02d0293743113f27e33d9825f
Muhammad Zeeshan Sohail <zeeshansohail17@gmail.com>
2018-08-20 12:13:10 +0500

    webservice request util

---
+++ urlshortener backend/src/main/java/com/gr/urlshortener/ws/util/WsRequestUtil.java
@@ -0,0 +1,17 @@
+package com.gr.urlshortener.ws.util;
+
+import javax.ws.rs.core.HttpHeaders;
+
+public class WsRequestUtil {
+
+	public static String getHeadersJson(HttpHeaders httpHeaders) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	public static String getPartnerGuid(HttpHeaders httpHeaders) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
