c8fd838f0166f4f931175c58f5190c231be0fcbc
Muhammad Zeeshan Sohail <zeeshansohail17@gmail.com>
2018-08-27 16:29:21 +0500

    sorted table according to clicks

--- urlshortener frontend/urlshortener/src/app/dashboard/dashboard.component.ts
+++ urlshortener frontend/urlshortener/src/app/dashboard/dashboard.component.ts
@@ -5,9 +5,9 @@ import { Url } from '../Url';
 @Component({
   selector: 'app-dashboard',
   templateUrl: './dashboard.component.html',
-  styleUrls: [ './dashboard.component.css' ]
+  styleUrls: ['./dashboard.component.css']
 })
- 
+
 
 export class DashboardComponent implements OnInit {
   urls: Url[] = [];
@@ -16,26 +16,27 @@ export class DashboardComponent implements OnInit {
   ngOnInit() {
     this.getUrls();
   }
-  
-  getUrls(): void{
+
+  getUrls(): void {
     this.urlService.getUrls().subscribe(urls => this.urls = urls);
   }
 
-  addShortUrl(urlString: string): void{
-    console.log("hello");
-    if(!urlString){
+  addShortUrl(urlString: string): void {
+    if (!urlString) {
       this.message = "Url cannot be Empty";
-    }else{
-      if((urlString.includes("http://")||(urlString.includes("https://"))) && urlString.includes(".com")){
-      this.urlService.addShortUrl(urlString).subscribe(url => this.urls.push(url));
-      this.message = "Url added Successfully";
-      location.reload();
-      console.log("else if");
-    }
-    else{
-      this.message = "invalid Url";
+    } else {
+      if ((urlString.includes("http://") || (urlString.includes("https://"))) && urlString.includes(".com")) {
+        this.urlService.addShortUrl(urlString).subscribe(url => this.urls.push(url));
+        this.message = "Url added Successfully";
+        location.reload();
+        console.log("else if");
+      }
+      else {
+        this.message = "invalid Url";
+      }
     }
   }
- 
+  filterBy(prop:  string)  {
+      return  this.urls.sort((a,  b)  =>  a[prop]  >  b[prop]  ?  1  :  a[prop]  ===  b[prop]  ?  0  :  -1);
   }
 }
